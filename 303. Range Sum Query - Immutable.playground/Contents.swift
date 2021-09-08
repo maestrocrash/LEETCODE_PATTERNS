d11beba8932c1b9bfb3e74d9ec2ae2c007b1cb3d
maestrocrash <maestrocrash@gmail.com>
2021-09-08 20:38:47 +0300

    One Variant -> Time Limit Exceeded

--- 303. Range Sum Query - Immutable.playground/Contents.swift
+++ 303. Range Sum Query - Immutable.playground/Contents.swift
@@ -2,11 +2,26 @@ import UIKit
 
 class NumArray {
 
+    var array: [Int] = []
+    
     init(_ nums: [Int]) {
-        
+        self.array = nums
     }
     
     func sumRange(_ left: Int, _ right: Int) -> Int {
+        var sum: Int = 0
+
+        for i in left...right {
+            sum += array[i]
+        }
         
+        return sum
     }
 }
+
+var arraySun = NumArray([-2, 0, 3, -5, 2, -1])
+
+arraySun.sumRange(0, 2)
+arraySun.sumRange(2, 5)
+arraySun.sumRange(0, 5)
+
